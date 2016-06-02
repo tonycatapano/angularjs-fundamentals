9396f353ead02e8c703645e97e5e0b7c82b45ad5
Tonino Catapano <t.catapano@tmg.nl>
2016-06-02 13:57:05 +0200

    new version from 04-ui-router-named-views branch

--- app/categories/categories.js
+++ app/categories/categories.js
@@ -1,33 +1,26 @@
 angular.module('categories', [
-  'eggly.models.categories'
+    'eggly.models.categories'
 ])
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('eggly.categories', {
-        url: '/',
-        views: {
-          'categories@': {
-            controller: 'CategoriesCtrl',
-            templateUrl: 'app/categories/categories.tmpl.html'
-          },
-          'bookmarks@': {
-            controller: 'BookmarksCtrl',
-            templateUrl: 'app/categories/bookmarks/bookmarks.tmpl.html'
-          }
-        }
-      });
-  })
+    .config(function($stateProvider){
+        $stateProvider
+            .state('eggly.categories', {
+                url: '/',
+                views: {
+                    'categories@': {
+                        controller: 'CategoriesCtrl',
+                        templateUrl: 'app/categories/categories.tmpl.html'
+                    },
+                    'bookmarks@': {
+                        controller: 'BookmarksCtrl',
+                        templateUrl: 'app/categories/bookmarks/bookmarks.tmpl.html'
+                    }
+                }
+            })
+    })
+    .controller('CategoriesCtrl', function CategoriesCtrl(){
 
-  .controller('CategoriesCtrl', function CategoriesCtrl($scope, categories) {
-    $scope.getCurrentCategoryName = categories.getCurrentCategoryName;
+    })
+    .controller('BookmarksCtrl', function BookmarksCtrl() {
 
-    categories.getCategories()
-      .then(function (result) {
-        $scope.categories = result;
-      });
-
-    $scope.isCurrentCategory = function (category) {
-      return category.name === $scope.getCurrentCategoryName();
-    }
-  })
+    })
 ;
