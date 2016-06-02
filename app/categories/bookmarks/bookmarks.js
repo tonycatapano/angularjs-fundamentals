86dfcf3b030a9900619990cd3f95761365046012
Tonino Catapano <t.catapano@tmg.nl>
2016-06-02 14:08:05 +0200

    removed all

--- app/categories/bookmarks/bookmarks.js
+++
@@ -1,6 +0,0 @@
-angular.module('categories.bookmarks', [
-    'categories.bookmarks.create',
-    'categories.bookmarks.edit',
-    'eggly.models.categories',
-    'eggly.models.bookmarks'
-]);
--- app/common/models/categories-model.js
+++
@@ -1,1 +0,0 @@
-angular.module('eggly.models.categories', []);
